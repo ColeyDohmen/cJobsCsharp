435975333e96cafd2f79b5bf7b7ab9391d22640c
ColeyDohmen <coley.dawn12@gmail.com>
2021-04-13 14:01:33 -0600

    starting up boi

---
+++ Repositories/JobsRepository.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using cJobs.Models;
+
+namespace cJobs.Repositories
+{
+    public class JobsRepository
+    {
+        internal IEnumerable<Job> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
