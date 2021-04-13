a87ac14eb05feefa3af1973ed7f4233a6c1c19d0
ColeyDohmen <coley.dawn12@gmail.com>
2021-04-13 17:06:02 -0600

    m2m 2

---
+++ Repositories/ContractorJobsRepository.cs
@@ -0,0 +1,18 @@
+using System;
+using cJobs.Models;
+
+namespace cJobs.Repositories
+{
+    public class ContractorJobsRepository
+    {
+        internal ContractorJob Create(ContractorJob newCJ)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
